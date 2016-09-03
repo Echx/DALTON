72a47ac5148b1dd20a8b1a299ea99d11b5efd795
Lei Mingyu <uygnim.iel@gmail.com>
2016-09-03 22:23:38 +0800

    force DdMad to follow Echx style guide

--- Dalton/DaltonColorCorrectExtension/Matrix.swift
+++ Dalton/DaltonColorCorrectExtension/Matrix.swift
@@ -9,40 +9,28 @@
 import Foundation
 
 class Matrix {
-    
     var matrix: [[Double]]
     
     init(matrix: [[Double]]) {
         self.matrix = matrix
     }
-    
 }
 
 func * (lhs: Matrix, rhs: Matrix) -> Matrix? {
-    
     if (lhs.matrix[0].count != rhs.matrix.count) {
-        
         print("Illegal matrix dimensions!")
         return nil
-        
     }
     
     var result:[[Double]] = [[Double]](count: lhs.matrix.count, repeatedValue:[Double](count: rhs.matrix[0].count, repeatedValue: 0))
     
     for i in 0 ..< result.count {
-        
         for j in 0 ..< result[0].count {
-            
             for k in 0 ..< rhs.matrix.count {
-                
                 result[i][j] += lhs.matrix[i][k] * rhs.matrix[k][j]
-                
             }
-            
         }
-        
     }
     
     return Matrix(matrix: result)
-    
 }
